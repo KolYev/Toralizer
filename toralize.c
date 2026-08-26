3e87719aefdbe043fb6324e845b7c9c644c01c03
KolYev <evgeniy.kolesenko@gmail.com>
2026-08-26 20:35:59 +0300

    Подготовка адресной структуры для IPv4

--- toralize.c
+++ toralize.c
@@ -2,7 +2,8 @@
 
 int main(int argc, char *argv[]) {
     char *host;
-    int port;
+    int port, s;
+    struct sockaddr_in sock;
 
     if (argc < 3) {
         fprintf(stderr, "Usage: %s <host> <port>\n",
@@ -13,4 +14,15 @@ int main(int argc, char *argv[]) {
 
     host = argv[1];
     port = atoi(argv[2]);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+        perror("socket");
+
+        return -1;
+    }
+
+    sock.sin_family = AF_INET;
+    sock.sin_port = htons(PROXYPORT);
+    sock.sin_addr.s_addr = inet_addr(PROXY);
 }
